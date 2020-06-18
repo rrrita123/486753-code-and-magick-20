41ebcafba7f29c0fff3afc764f9d4db2892019d2
Margo <rrrita123@mail.ru>
2020-06-18 22:04:55 +0400

    Учебный проект модули

---
+++ js/dialog.js
@@ -0,0 +1,91 @@
+'use strict';
+
+(function () {
+  // Открытие/закрытие окна настройки персонажа
+  var FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+  var setupOpen = document.querySelector('.setup-open');
+  var userPopup = document.querySelector('.setup');
+  var setupClose = document.querySelector('.setup-close');
+  var inputName = document.querySelector('.setup-user-name');
+  var isNotFocus = true;
+
+  // Обработчик фокуса на input
+  var onInputFocus = function () {
+    isNotFocus = false;
+  };
+
+  var onInputNotFocus = function () {
+    isNotFocus = true;
+  };
+
+  var wizardCoat = userPopup.querySelector('.setup-wizard .wizard-coat');
+  var wizardEyes = userPopup.querySelector('.setup-wizard .wizard-eyes');
+  var wizardFireball = userPopup.querySelector('.setup-fireball-wrap');
+
+  // Обработчик выбора цвета мантии
+  var onCoatChangeColor = function () {
+    var colorCoat = window.colorize.changeColor(wizardCoat, window.setup.COAT_COLOR);
+    userPopup.querySelector('input[name=coat-color]').value = colorCoat;
+  };
+
+  // Обработчик выбора цвета глаз
+  var onEyesChangeColor = function () {
+    var colorEyes = window.colorize.changeColor(wizardEyes, window.setup.EYES_COLOR);
+    userPopup.querySelector('input[name=eyes-color]').value = colorEyes;
+  };
+
+  // Обработчик выбора цвета фаербола
+  var onFireballChangeColor = function () {
+    var colorFireball = window.colorize.changeColor(wizardFireball, FIREBALL_COLOR);
+    userPopup.querySelector('input[name=fireball-color]').value = colorFireball;
+  };
+
+  var openPopup = function () {
+    userPopup.classList.remove('hidden');
+
+    document.addEventListener('keydown', onPopupEscPress);
+    inputName.addEventListener('focus', onInputFocus);
+    inputName.addEventListener('blur', onInputNotFocus);
+    wizardCoat.addEventListener('click', onCoatChangeColor);
+    wizardEyes.addEventListener('click', onEyesChangeColor);
+    wizardFireball.addEventListener('click', onFireballChangeColor);
+  };
+
+  var closePopup = function () {
+    userPopup.classList.add('hidden');
+
+    document.removeEventListener('keydown', onPopupEscPress);
+    document.removeEventListener('focus', onInputFocus);
+    document.removeEventListener('blur', onInputNotFocus);
+    wizardCoat.removeEventListener('click', onCoatChangeColor);
+    wizardEyes.removeEventListener('click', onEyesChangeColor);
+    wizardFireball.removeEventListener('click', onFireballChangeColor);
+
+    userPopup.style.removeProperty('top');
+    userPopup.style.removeProperty('left');
+  };
+
+  // Обработчик окна на ESC
+  var onPopupEscPress = function (evt) {
+    window.util.isEscEvent(evt, closePopup, isNotFocus);
+  };
+
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  // Открывает диалоговое окно по Enter
+  setupOpen.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, openPopup);
+  });
+
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  // Закрывает диалоговое окно по иконке Х Enter
+  setupClose.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, closePopup);
+  });
+})();
